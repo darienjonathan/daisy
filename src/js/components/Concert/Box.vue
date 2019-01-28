497cf667ed006bfd1672ef9450d66a6eea4e0e44
darienjonathan <darien-winata@kayac.com>
2019-01-28 14:44:00 +0900

    move box css to box component

--- src/js/components/Concert/Box.vue
+++ src/js/components/Concert/Box.vue
@@ -13,6 +13,27 @@
 
   height: 550px;
 }
+
+.box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+
+  width: 100vw;
+  height: 550px;
+
+  position: fixed;
+  top: 50%;
+  transform: translateY(-50%);
+
+  background-color: rgba(255,255,255,0.85);
+
+  box-sizing: border-box;
+
+  transition: opacity 0.5s linear;
+}
+
 </style>
 
 <script>
